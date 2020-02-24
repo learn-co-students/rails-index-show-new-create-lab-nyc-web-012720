9bb2ede23c2c5ed4c0565b0cf1910ee55e75b526
adit313 <app2116@gmail.com>
2020-02-24 15:50:45 -0500

    Done.

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -1,19 +1,53 @@
 require 'rails_helper'
 
-describe CouponsController do
+RSpec.describe CouponsController, type: :controller do
 
-  describe "POST create" do
-    context "with valid attributes" do
-      it "creates a new coupon" do
-        expect{
-          post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
-        }.to change(Coupon,:count).by(1)
-      end
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      it "redirects to the new coupon" do
-        post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
-        expect(response).to redirect_to Coupon.last
-      end
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #destroy" do
+    it "returns http success" do
+      get :destroy
+      expect(response).to have_http_status(:success)
     end
   end
 
